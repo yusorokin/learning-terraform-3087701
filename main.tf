c12104e44fca6ff0057cb6cd0a1ba941b64f96c7
Yuriy <ysorokin.contractor@libertyglobal.com>
2024-02-05 21:44:42 +0600

    Move to GCP

--- main.tf
+++ main.tf
@@ -1,24 +1,53 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+data "google_compute_image" "my_image" {
+  family  = "debian-11"
+  project = "debian-cloud"
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+resource "google_compute_instance" "instance-1" {
+  boot_disk {
+    auto_delete = true
+    device_name = "instance-1"
 
-  tags = {
-    Name = "HelloWorld"
+    initialize_params {
+      image = data.google_compute_image.my_image.self_link
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
   }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  machine_type = "f1-micro"
+
+  name = "instance-1"
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/dynamic-density-246618/regions/europe-west1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+    instance_termination_action = "STOP"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "europe-west1-b"
 }
--- providers.tf
+++ providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+    google = {
+      source  = "hashicorp/google"
     }
   }
 }
 
-provider "aws" {
-  region  = "us-west-2"
+provider "google" {
+  project = "dynamic-density-246618"
+  region  = "europe-west1"
 }
